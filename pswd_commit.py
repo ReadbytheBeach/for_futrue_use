a5f155376ff9c4d8e566dfa92c8be522c18bfcf9
ReadbytheBeach <Heinekenblue@163.com>
2023-01-07 22:12:46 +0800

    change print content

--- pswd_commit.py
+++ pswd_commit.py
@@ -2,12 +2,12 @@ while True:
     print(('who are you?\n') +'Input your name: ')
     name_input = input().lower()
     if name_input != 'xj':
-        print('Put wrong name. Again!')
+        print('Wrong user. Again!')
         continue
 
     print('Hello xj, what is the password (It is a plant in your department)')
     pswd_input = input().lower()
     if pswd_input == 'cereus':
         break
-    print('Put wrong password. Again!')
+    print('Wrong password. Again!')
 print ('Access granted.')
