895674b4dca61c07cff01dc6e881f48855586f2a
ReadbytheBeach <Heinekenblue@163.com>
2023-01-10 21:56:21 +0800

    use enumerate() instead for i in range()

--- input_names.py
+++ input_names.py
@@ -7,7 +7,12 @@ while True:
         break # exit the while
     catNames = catNames + [name]  # list connect 这个技巧之前没用过
 print('The cat names are:')
-for name in catNames:
-    print(''+ name)
+# for name in catNames:
+#     print(''+ name)
+'''
+用 enumerate 改写以上的代码
+'''
+for index, item in enumerate(catNames):
+    print('Index '+ str(index) + 'cat name is: ' + item  )
+
 print('Totally '+str(len(catNames))+' cat names.')
-print(catNames)
