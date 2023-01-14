f31f0bfecc6ba8c8a64a43c4be2c03e3b8a09dc0
ReadbytheBeach <Heinekenblue@163.com>
2023-01-14 21:39:23 +0800

    add dictionary_setdefault_static_charaters.py

---
+++ dictionary_setdefault_static_charaters.py
@@ -0,0 +1,7 @@
+count = {} # 统计使用一个数组，key是字母，value是出现的次数
+count_string = 'this is a very smart way to use setdefault() to count the character in a string'
+for character in count_string:
+    count.setdefault(character, 0)  # 如果字符还不在字典的key中， 则设置新key = charater, 并赋予初始值0
+    count[character] = count[character] + 1
+
+print(count)
