79fdd969586a4ebef702299bad0a7a2056208f6f
ReadbytheBeach <Heinekenblue@163.com>
2023-01-23 09:42:44 +0800

    add find_longest_string_in_nested_list.py

---
+++ find_longest_strin_in_nested_list.py
@@ -0,0 +1,19 @@
+tableDate = [['apple','orange','cherries','banana'],['Alice','Bob','Carol','David'],['dogs','cat','moose','goose']]
+print(tableDate)
+
+# 以下循环找出嵌套列表的最长字符串
+colWidths = [0] * len(tableDate)
+print(colWidths)
+for i in range(len(tableDate)):
+    len_elem = 0
+    for j in range (len(tableDate[i])):  # 这样的表达方式尽然可以实现列表的嵌套， 即tableDate[i] == tableDate[i][:]
+        item_len = len(tableDate[i][j])
+        # 如果单词长度大于现有的最大长度记录，那最大记录替换成现有长度
+        if item_len > len_elem:
+            len_elem = item_len
+    colWidths[i] = len_elem # 每一次的循环结尾收集最大的单词长度
+# print(colWidths)
+max_len = max(colWidths)
+# print(max_len)
+
+    
